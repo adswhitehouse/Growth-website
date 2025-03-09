7f1668f8e74d442940ced20c7ed392827fcd0f0b
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-09 14:45:37 +0000

    added pseudo states css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Variables
 let faqs = document.querySelectorAll(".jsFaqPara");
 let expandFaqs = document.querySelectorAll(".fa-plus");
+let filterBtns = document.querySelectorAll(".jsFilterBtn");
 
 // Toggles FAQ answer visibility, x icon and after pseudo color on plus icon click
 expandFaqs.forEach((expander) => {
@@ -9,5 +10,15 @@ expandFaqs.forEach((expander) => {
     expander.parentElement.classList.toggle("question-active-after");
     expander.classList.toggle("fa-plus");
     expander.classList.toggle("fa-x");
+  });
+});
+
+// When an FAQ button is clicked the active state is applied and removed from all other buttons
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    filterBtns.forEach((btn) => {
+      btn.classList.remove("filter-btn-active");
+    });
+    btn.classList.add("filter-btn-active");
   });
 });
