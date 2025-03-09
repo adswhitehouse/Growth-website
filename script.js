80b059f5f38800e535eacc49f51f1a322c00a95d
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-09 09:06:05 +0000

    worked on faq section, added js comments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Variables
-let faqs = document.querySelectorAll(".jsFaqPara")
-let expandFaqs = document.querySelectorAll(".fa-plus")
+let faqs = document.querySelectorAll(".jsFaqPara");
+let expandFaqs = document.querySelectorAll(".fa-plus");
 
+// Toggles FAQ answer visibility, x icon and after pseudo color on plus icon click
 expandFaqs.forEach((expander) => {
   expander.addEventListener("click", () => {
-    expander.previousElementSibling.classList.toggle("question-active")
-  })
-})
+    expander.previousElementSibling.classList.toggle("question-active");
+    expander.parentElement.classList.toggle("question-active-after");
+    expander.classList.toggle("fa-plus");
+    expander.classList.toggle("fa-x");
+  });
+});
